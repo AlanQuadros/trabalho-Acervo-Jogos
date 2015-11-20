77fb0efd1e326de5883c4fe33e72551287a0a507
ALAN OLIVEIRA DE QUADROS <15280420@l1847110.portoalegre.pucrsnet.br>
2015-11-20 15:33:30 -0200

    Classes Genero e Plataforma

---
+++ src/dbapplication/Plataforma.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbapplication;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Alan Quadros
+ */
+public class Plataforma {
+    private long idPlataforma;
+    private String nomePlataforma;
+    private String tipoPlataforma;
+    private String fabricante;
+    private Date dataLancamento;
+
+    public Plataforma() {
+    }
+
+    public long getIdPlataforma() {
+        return idPlataforma;
+    }
+
+    public void setIdPlataforma(long idPlataforma) {
+        this.idPlataforma = idPlataforma;
+    }
+
+    public String getNomePlataforma() {
+        return nomePlataforma;
+    }
+
+    public void setNomePlataforma(String nomePlataforma) {
+        this.nomePlataforma = nomePlataforma;
+    }
+
+    public String getTipoPlataforma() {
+        return tipoPlataforma;
+    }
+
+    public void setTipoPlataforma(String tipoPlataforma) {
+        this.tipoPlataforma = tipoPlataforma;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public Date getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(Date dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Código Plataforma: "+idPlataforma
+                +"\nNome: "+nomePlataforma
+                +"\nTipo: "+tipoPlataforma
+                +"\nFabricante: "+fabricante
+                +"\nData de lançamento: "+dataLancamento;
+    }
+}
