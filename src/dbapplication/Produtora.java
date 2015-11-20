8707e618ae2d675745a4d872e00758a1d671b4a0
JOAO VITOR DA S. B. DOS SANTOS <15202607@l1847078.portoalegre.pucrsnet.br>
2015-11-20 15:32:12 -0200

    class

---
+++ src/dbapplication/Produtora.java
@@ -0,0 +1,58 @@
+package dbapplication;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author João Vitor
+ */
+public class Produtora {
+
+    private long id;
+    private String nome, pais, site;
+
+    public Produtora() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getSite() {
+        return site;
+    }
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+    
+    public String toString(){
+        return "Id: " + id + "Nome: " + nome + "País: " + pais + "Site: " + site;
+    }
+    
+}
